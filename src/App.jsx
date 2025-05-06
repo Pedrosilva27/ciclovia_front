90fa7834178c50dcfa3c69fefaa213f9fab8b881
Pedro Henrique Pereira da Silva <pedro.mmacomabt@gmail.com>
2025-05-05 22:45:58 -0300

    Estrutura básica

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div>
+      <h1>Ola mundo</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,5 @@
+function Map() {
+  return <h1>Card</h1>;
+}
+
+export default Map;
